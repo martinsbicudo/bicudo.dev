b429c56da20c57981545ff1b5a455e595b31fd6f
Victor Martins Bicudo <victor_bicudo@outlook.com>
2020-07-01 21:56:03 -0300

    - Remove import that doesn't exist

--- components/common/index.js
+++ components/common/index.js
@@ -1,4 +1,3 @@
-export { default as Button } from './Button';
 export { default as Choice } from './Choice';
 export { default as Container } from './Container';
 export { default as Date } from './Date';
